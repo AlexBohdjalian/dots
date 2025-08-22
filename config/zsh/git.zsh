117dd09d68b162f24c1f5b32efbe1ec6bc302939
AlexBohdjalian <alexandros11@hotmail.co.uk>
2025-08-22 16:47:04 +0100

    chore: sync with fork

--- config/zsh/git.zsh
+++ config/zsh/git.zsh
@@ -1,23 +1,2 @@
-alias gs='git status'
-# alias gl='git log --oneline --graph --decorate'
-alias gla='git log --oneline --graph --decorate --all'
-alias gb='git branch'
-alias gd='git diff'
-
-alias ga='git add'
-alias gap='git add -p'
-alias gc='git commit'
-alias gcmsg='git commit -m'
-alias gco='git checkout'
-alias gcb='git checkout -b'
-alias gp='git push'
-alias gl='git pull'
-
-alias gcp='git cherry-pick'
-alias gst='git stash'
-alias gundo='git reset --soft HEAD~'
-alias greset='git reset --hard HEAD'
-
-alias gf='git fetch'
-
-alias gm='git merge'
+zinit ice wait"1" lucid
+zinit snippet OMZP::git
